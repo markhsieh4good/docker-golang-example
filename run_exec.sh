a3ceab386b9a45f3e3d6460192f94d52c5b0e10e
markhsieh4good <79235892+markhsieh4good@users.noreply.github.com>
2021-06-16 13:47:03 +0800

    Update run_exec.sh

--- run_exec.sh
+++ run_exec.sh
@@ -2,5 +2,7 @@
 
 _NAME=$1
 _EXEC=$2
+
+echo "we create container [${_NAME}] to run exec [${_EXEC}]"
 sudo docker run --restart always -d --name ${_NAME} -v ${PWD}/redshark_fakeapi:/opt -w /opt -p 8051:8051 golang:1.15.5-modify ./${_EXEC}
 echo "running exec ..."
